e197cc774b2fe3c9722c2f26e41f74aa8dfd3d5c
Ravi <samcollins9899@gmail.com>
2023-05-24 03:29:58 +0530

    added error pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
-import Landing from "./pages/Landing"
+
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
-
+import {Dashboard, Landing, Error, Register} from './pages/Index';
 function App() {
   
 
@@ -9,10 +9,10 @@ function App() {
     <BrowserRouter>
         <Routes>
          
-         <Route path="/" element={<div>Dashboard</div>} />
-         <Route path="register" element={<div>Register</div>} />
+         <Route path="/" element={<Dashboard /> } />
+         <Route path="register" element={<Register />} />
          <Route path="/landing" element={<Landing />} />
-         <Route path ="*" element={<h1>Error</h1>} />
+         <Route path ="*" element={<Error />} />
 
         </Routes>
        
