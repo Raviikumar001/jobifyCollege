4af1f878f9f7a1bea4558e15b327cb205f867b30
Ravi <samcollins9899@gmail.com>
2023-05-26 10:31:19 +0530

    added fix1

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+
+import { useState, useEffect } from "react"
+import {Logo} from '../components/Index';
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember:  true,
+}
+
 
 const Register = () => {
+
+  const [values, setValues] = useState(initialState);
+
+  const handleChage = (e)=> {
+     console.log(e.target);
+
+  }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  }
+
+
   return (
-    <h1>Register </h1>
+    <Wrapper className="full-page">
+      <form  className="form" onSubmit={onSubmit}>
+      <Logo />
+      <h3>Login</h3>
+       <div className="form-row"> 
+       <label htmlFor="name" className="form-lable">
+       Name
+
+       </label>
+       <input   type="text" value ={values.name} name="name"
+
+        onChange={handleChage}
+        className="form-input"
+       />
+
+       </div>
+       <button type="submit" className="btn btn-block">
+          Submit
+       </button>
+
+      </form>
+    </Wrapper>
   )
 }
 
